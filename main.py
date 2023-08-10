cfa9eefe175f9140baaecbf44069a9858013d6e2
Merisa_P <rice1006@gmail.com>
2023-08-10 16:11:57 +0700

    update code

--- main.py
+++ main.py
@@ -1,116 +1,104 @@
 import cv2
 import cvzone
-import pandas as pd
-import numpy as np
 import math
+import argparse
+import numpy as np
+import supervision as sv
+# import pandas as pd
+# import torch
 # import time
 
 from ultralytics import YOLO
 
-# YOLO's model
 model = YOLO('Yolo-Weights/yolov8n.pt')
 
-# Class name object
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"]
-
-def RGB(event, x, y, flags, param):
+def Display(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE:
         colorsBGR = [x, y]
         print(colorsBGR)
 
 
-cv2.namedWindow('RGB')
-cv2.setMouseCallback('RGB', RGB)
+cv2.namedWindow('Display')
+cv2.setMouseCallback('Display', Display)
 
-# set camera
-cap = cv2.VideoCapture('Videos/bikes.mp4')
-# cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
-
+# Class name object
 my_file = open("coco.txt", "r")
 data = my_file.read()
-class_list = data.split("\n")
+classNames = data.split("\n")
 
-# Custom area for detection
-area9 = [(511, 327), (557, 388), (603, 383), (549, 324)]
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+# Frame set
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Camera")
+    parser.add_argument(
+        "--camera-resolution",
+        default=[1280, 720],
+        nargs=2,
+        type=int
+    )
+    args = parser.parse_args()
+    return args
 
-    frame = cv2.resize(frame, (1280, 720))
 
-    results = model.predict(frame)
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
+# polygon zone
+ZONE_POLYGON = np.array([
+    [1725, 1550],
+    [2725, 1550],
+    [3500, 2160],
+    [1250, 2160]
+])
+# video_info = sv.VideoInfo.from_video_path(MALL_VIDEO_PATH)  # Use path of video
+# zone = sv.PolygonZone(polygon=polygon, frame_resolution_wh=video_info.resolution_wh)
 
-            # Bounding Box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # for CV2
-            # cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
-            # for cvzone
-            w, h = x2-x1, y2-y1
-            cvzone.cornerRect(frame, (x1, y1, w, h))
-            # Confidence
-            conf = math.ceil((box.conf[0]*100))/100
 
-            # Class Name
-            cls = int(box.cls[0])
+def main():
+    args = parse_arguments()
+    frame_width, frame_height = args.camera_resolution
 
-            cvzone.putTextRect(frame, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
+    # cap = cv2.VideoCapture('Videos/cars.mp4')  # Video
+    cap = cv2.VideoCapture(0)  # Camera
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
-    #   print(results)
-    a = results[0].boxes.boxes
-    px = pd.DataFrame(a).astype("float")
-    #    print(px)
+    # zone = sv.PolygonZone(polygon=ZONE_POLYGON, frame_resolution_wh=tuple(args.camera_resolution))
+    # zone_annotator = sv.PolygonZoneAnnotator(zone=zone, color=sv.Color.white())
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        results = model(frame, stream=True)
+        # frame = zone_annotator.annotate(scene=frame)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                # Bounding Box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                # for CV2
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 1)
+                # for cvzone
+                w, h = x2 - x1, y2 - y1
+                # Confidence
+                conf = math.ceil((box.conf[0] * 100)) / 100
+                # Class Name
+                cls = int(box.cls[0])
+                currentClass = classNames[cls]
+                # Detect an object
+                if currentClass == "car" and conf > 0.3:
+                    cvzone.putTextRect(frame,
+                                       f'{currentClass} {conf}',
+                                       (max(0, x1),
+                                        max(35, y1)),
+                                       scale=1,
+                                       thickness=1,
+                                       offset=5)
+                    cvzone.cornerRect(frame, (x1, y1, w, h), l=10)
+        cv2.imshow("Display", frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
-    list9 = []
 
-    for index, row in px.iterrows():
-        #        print(row)
-
-        x1 = int(row[0])
-        y1 = int(row[1])
-        x2 = int(row[2])
-        y2 = int(row[3])
-        d = int(row[5])
-        c = class_list[d]
-        if 'car' in c:
-            cx = int(x1 + x2) // 2
-            cy = int(y1 + y2) // 2
-
-            results9 = cv2.pointPolygonTest(np.array(area9, np.int32), ((cx, cy)), False)
-            if results9 >= 0:
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.circle(frame, (cx, cy), 3, (0, 0, 255), -1)
-                list9.append(c)
-
-    a9 = (len(list9))
-
-    if a9 == 1:
-        cv2.polylines(frame, [np.array(area9, np.int32)], True, (0, 0, 255), 2)
-        cv2.putText(frame, str('9'), (591, 398), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 1)
-    else:
-        cv2.polylines(frame, [np.array(area9, np.int32)], True, (0, 255, 0), 2)
-        cv2.putText(frame, str('9'), (591, 398), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
-
-    cv2.imshow("RGB", frame)
-
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
-cap.release()
-cv2.destroyAllWindows()
-# stream.stop()
+if __name__ == "__main__":
+    main()
